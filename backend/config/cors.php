8953cb365964bc84f3c27b57d189b62b12ffeb3a
Reykira <reychan.kirana@gmail.com>
2026-09-04 18:57:43 +0700

    chore(deployment): configure production cors

---
+++ backend/config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'], // Allow all origin (Vercel & Domain Custom)
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false, // False agar wildcard '*' diizinkan browser untuk Authorization Bearer Token
+];
